6ec5c83ad7bbdf6d52d31162e1eed1d689533ae1
Gunesh Pinar <gkpinar@uwaterloo.ca>
2020-04-28 22:16:52 -0400

    Retrieve Summoner puuid Using -track Command

--- README.md
+++ README.md
@@ -2,15 +2,18 @@
 - https://www.digitaltrends.com/gaming/how-to-make-a-discord-bot/ (Bot Tutorial)
 - https://medium.com/davao-js/2019-tutorial-creating-your-first-simple-discord-bot-47fc836a170b (Bot Tutorial)
 - https://medium.com/davao-js/v2-tutorial-deploy-your-discord-bot-to-heroku-part-2-9a37572d5de4 (Hosting)
-
+- https://discord.js.org/#/docs/main/stable/general/welcome (discord.js Documentation)
+- https://www.npmjs.com/package/twisted (LOL API Wrapper for Node.js)
 
 ## Running the Bot
 1) Obtain bot token from Discord Developer Portal
-2) Create `.env` file in the main repository in the following format:
+2) Obtain Riot API Key from Riot Developer Portal
+3) Create `.env` file in the main repository in the following format:
     ```
+    RIOT_API_KEY=<YOUR RIOT API KEY>
     DISCORD_TOKEN=<YOUR DEVELOPER TOKEN>
     ```
-3) Start bot with
+4) Start bot with
     ```
     node bot.js
     ```
--- bot.js
+++ bot.js
@@ -1,7 +1,13 @@
-require('dotenv').config();
+import dotenv from 'dotenv'
+import twisted from 'twisted'
+import Discord from 'discord.js'
 
-const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = "-tftbot ";
+const TftApi = new twisted.TftApi();
+const Constants = twisted.Constants;
+
+dotenv.config()
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -9,8 +15,103 @@ client.on('ready', () => {
 
 client.login(process.env.DISCORD_TOKEN);
 
-client.on('message', msg => {
-    if (msg.content === 'ping') {
-      msg.reply('pong');
-    }
+
+// TODO: Currently this bot only works with a single channel. It should work with multiple.
+var channelId;
+
+client.on('message', message => {
+  channelId = message.channel.id;
+  console.log(channelId);
+
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).split(' ');
+  const command = args.shift().toLowerCase();
+
+  console.log(`Command name: ${command}\nArguments: ${args}`);
+
+  switch(command){
+    case 'track':
+      trackSummoner(args, message.channel);
+      return;
+    default:
+      return;
+  }
 });
+
+class Summoner {
+  constructor(name, puuid, region, TFTRegion) {
+    this.name = name;
+    this.puuid = puuid;
+    this.region = region;
+    this.TFTRegion = TFTRegion;
+  }
+}
+
+var trackedSummoners = [];
+
+function trackSummoner(args, channel) {
+  const availableRegions = ["na", "euw"];
+  const userRegion = args[1].toLowerCase();
+  const summonerName = args[0];
+
+  if (args.length != 2 || !availableRegions.includes(userRegion)) {
+    channel.send('Incorrect usage of command \'track\'.\nCorrect usage: -tftbot track <summoner name> <region (na/euw)>');
+    return;
+  }
+
+  if (trackedSummoners.length == 5){
+    channel.send('Already tracking 5 summoners. Use \'list\' to display tracked summoners or \'clear\' to clear the list.');
+    return;
+  }
+
+  const twistedRegion = regionArgToTwistedRegion(userRegion);
+
+  getTFTSummoner(summonerName, twistedRegion)
+    .then(puuid => {
+      console.log('Received puuid:', puuid);
+      const newSummoner = new Summoner(summonerName, puuid, twistedRegion, Constants.regionToTftRegions(twistedRegion));
+      trackedSummoners.push(newSummoner);
+      console.log('Tracking Summoner:', newSummoner);
+      channel.send(puuid);
+    })
+    .catch(error => { 
+      if (error.status == 404){
+        channel.send(`Summoner ${summonerName} does not exist in region ${userRegion}.`);
+      }
+      console.log('Caught Error:', error.message); 
+    });
+}
+
+function regionArgToTwistedRegion(regionArg){
+  switch(regionArg){
+    case 'na':
+      return Constants.Regions.AMERICA_NORTH;
+    case 'euw':
+      return Constants.Regions.EU_WEST;
+    default:
+      return;
+  }
+}
+
+async function getTFTSummoner (summonerName, twistedRegion) {
+  const {
+    response: {
+      puuid 
+    }
+  } = await TftApi.Summoner.getByName(summonerName, twistedRegion)
+
+  return puuid;
+}
+
+// export async function getTFTMatchList (puuid, TFTRegion) {
+//   const {
+//     response: {
+//       puuid 
+//     }
+//   } = await TftApi.Summoner.getByName(summonerName, regionArgToTwistedRegion(region))
+
+//   return TftApi.Match.list(puuid, Constants.TftRegions.AMERICAS);
+// }
+
+
